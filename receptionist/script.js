0257aef199853c9255b250de5ece4e1990fee823
raymondfanuel <raymondfanuel1@gmail.com>
2025-08-08 11:40:44 +0300

    including js

---
+++ receptionist/script.js
@@ -0,0 +1,11 @@
+function registerPatient() {
+    window.location.href = "register_patient.php";
+}
+
+function viewPatients() {
+    window.location.href = "view_patients.php";
+}
+
+function bookAppointment() {
+    window.location.href = "book_appointment.php";
+}
